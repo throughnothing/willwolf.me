88602239ebd816c1b2bbd13a302bad032484dddf
William Wolf <throughnothing@gmail.com>
2017-04-08 13:09:36 -0700

    Initial Hakyll Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# throughnothing.com
+
+This site is built using [Hakyll](https://jaspervdj.be/hakyll/).
+
+```
+make build # Build the site
+make rebuild # Clean + Build
+make serve # Serve local copy
+make publish # Publish
+```
---
+++ src/site.hs
@@ -0,0 +1,77 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+--------------------------------------------------------------------------------
+
+configuration :: Configuration
+configuration = defaultConfiguration
+    { destinationDirectory = "../output/"
+    , storeDirectory = "../.hakyll-cache"
+    }
+
+main :: IO ()
+main = hakyllWith configuration $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ compile compressCssCompiler
+
+    -- Combine all CSS
+    create ["all.css"] $ do
+        route idRoute
+        compile $ do
+            items <- loadAll "css/*" :: Compiler [Item String]
+            makeItem $ concatMap itemBody items
+
+    match (fromList ["about.rst", "contact.markdown"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+
+    match "index.markdown" $ do
+        route   $ setExtension "html"
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    defaultContext
+
+            -- pandocCompiler
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateBodyCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
