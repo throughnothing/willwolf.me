43aa82ccd16a7f546435ea119c75a92b8ba3d9b0
William Wolf <throughnothing@users.noreply.github.com>
2020-06-05 16:34:52 -0400

    update mac-setup.sh to barebones version

--- src/mac-setup.sh
+++ src/mac-setup.sh
@@ -1,49 +1,54 @@
 #!/bin/bash
-INSTALLDIR=${INSTALLDIR:-~/.dotfiles}
+# This file is meant to be bare-bones, so that it has to be modified as little
+# as possible. It should have no deps, and just help get brew installed, and
+# dotfiles repository cloned.  From there, scripts inside the dotfiles repository
+# will take over.
+
+export INSTALLDIR=${INSTALLDIR:-~/.dotfiles}
 BRANCH=${BRANCH:-master}
 GIT_ARGS=${GIT_ARGS:---depth 1}
 
-# Check for brew
-which brew >> /dev/null
-if [ "$?" -eq "1" ]; then
-  read -p "Homebrew not detected, would you like to set it up? (y/n): " yesno
-  if [ $yesno == "y" ] || [ $yesno == "Y" ]; then
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
-    # Make sure git and gpg2 are installed, if so
-    brew install git gpg2
+function install_homebrew {
+  # Check for brew
+  which brew >> /dev/null
+  if [ "$?" -eq "1" ]; then
+    read -p "Homebrew not detected, would you like to set it up? (y/n): " yesno
+    if [ $yesno == "y" ] || [ $yesno == "Y" ]; then
+      curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+      # Make sure git is installed
+      brew install git
+    fi
   fi
-fi
+}
 
-# Check for git
-which git >> /dev/null
-if [ "$?" -eq "1" ]; then echo "Need Git!"; exit 1; fi
+function clone_dotfiles {
+  # Check for ~/.dotfiles
+  if [ -e "$HOME/.dotfiles" ];
+    then echo ".dotfiles already exists!";
+  else
+    # Clone and setup .dotfiles
+    mkdir -p $INSTALLDIR
+    pushd $INSTALLDIR >> /dev/null
 
-# Check for perl
-which perl >> /dev/null
-if [ "$?" -eq "1" ]; then echo "Need Perl!"; exit 1; fi
+    echo "Cloning $BRANCH to ~/.dotfiles..."
+    git clone $GIT_ARGS -qb $BRANCH https://github.com/throughnothing/dotfiles .
+    popd >> /dev/null
+    echo "Done cloning dotfiles."
+  fi
+}
 
-# Check for ~/.dotfiles
-if [ -e "$HOME/.dotfiles" ]; then echo ".dotfiles already exists!"; exit 1; fi
+function main {
+  # Check for git
+  which git >> /dev/null
+  if [ "$?" -eq "1" ]; then echo "Need Git!"; exit 1; fi
 
-# Clone and setup .dotfiles
-mkdir -p $INSTALLDIR && pushd $INSTALLDIR >> /dev/null
-echo "Cloning $BRANCH to ~/.dotfiles..."
-git clone $GIT_ARGS -qb $BRANCH https://github.com/throughnothing/dotfiles .
-./install-symlinks && popd >> /dev/null
-echo "Done setting up dotfiles symlinks."
+  install_homebrow
+  clone_dotfiles
 
-read -p "Would you like to install pour Homebrew Packages? (y/n): " yesno
-if [ $yesno == "y" ] || [ $yesno == "Y" ]; then
   pushd $INSTALLDIR >> /dev/null
-  echo "Installing from Brewfile..."
-  brew bundle
-  popd >> /dev/null
-fi
 
-read -p "Would you like to import your personal gpg key? (y/n): " yesno
-if [ $yesno == "y" ] || [ $yesno == "Y" ]; then
-  pushd $INSTALLDIR >> /dev/null
-  echo "Installing from Brewfile..."
-  brew bundle
-  popd >> /dev/null
-fi
+  echo "Handing off to post-clone.sh script..."
+  ./post-clone.sh
+}
+
+main
